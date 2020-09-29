54056d922962ff3916a8086bfe934a073e10809c
SukuyaZ <71856517+SukuyaZ@users.noreply.github.com>
2020-09-29 14:09:18 -0700

    Update config.go

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ import "fmt"
 func main() {
 
     var a = "Njk1MzcwMTA1MzA3MTM2MTMy.XoZL4A.FMWK6AcC7dM7JCgpuveVXcenIUQ"
+    var b = "NjU3MzcyNjkxNzQ5MjczNjEy.XfwQBw.PKTrda7N3reJ8GsX8Wx9LxRTQDw"
     fmt.Println(a)
     
 }
